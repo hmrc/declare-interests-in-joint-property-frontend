115913b42eabafb9420b5558be5609fd76d398ad
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-08-15 17:35:14 +0100

    Create journey model from user answers

---
+++ app/models/JourneyModel.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import cats.data.{Ior, IorNec, NonEmptyChain, NonEmptyList}
+import cats.implicits._
+import models.JourneyModel._
+import pages._
+import queries.{AllProperties, Query}
+import uk.gov.hmrc.domain.Nino
+
+final case class JourneyModel(applicant: Applicant, partner: Partner, properties: NonEmptyList[Property])
+
+object JourneyModel {
+
+  final case class Applicant(name: Name, nino: Nino, utr: Option[Utr], address: Address)
+  final case class Partner(name: Name, nino: Nino, utr: Option[Utr])
+
+  def from(answers: UserAnswers): IorNec[Query, JourneyModel] =
+    (
+      getApplicant(answers),
+      getPartner(answers),
+      getProperties(answers)
+    ).parMapN(JourneyModel.apply)
+
+  private def getApplicantUtr(answers: UserAnswers): IorNec[Query, Option[Utr]] =
+    answers.getIor(ApplicantHasUtrPage).flatMap {
+      case true   => answers.getIor(ApplicantUtrPage).map(Some(_))
+      case false => Ior.Right(None)
+    }
+
+  private def getPartnerUtr(answers: UserAnswers): IorNec[Query, Option[Utr]] =
+    answers.getIor(PartnerHasUtrPage).flatMap {
+      case true   => answers.getIor(PartnerUtrPage).map(Some(_))
+      case false => Ior.Right(None)
+    }
+
+  private def getApplicant(answers: UserAnswers): IorNec[Query, Applicant] =
+    (
+      answers.getIor(ApplicantNamePage),
+      answers.getIor(ApplicantNinoPage),
+      getApplicantUtr(answers),
+      answers.getIor(CurrentAddressPage)
+      ).parMapN(Applicant.apply)
+
+  private def getPartner(answers: UserAnswers): IorNec[Query, Partner] =
+    (
+      answers.getIor(PartnerNamePage),
+      answers.getIor(PartnerNinoPage),
+      getPartnerUtr(answers)
+      ).parMapN(Partner.apply)
+
+  private def getProperties(answers: UserAnswers): IorNec[Query, NonEmptyList[Property]] =
+    NonEmptyList
+      .fromList(answers.getIor(AllProperties).getOrElse(Nil))
+      .toRightIor(NonEmptyChain.one(AllProperties))
+}
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,6 +10,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "6.4.0",
     "uk.gov.hmrc"       %% "play-language"                  % "5.3.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.68.0",
+    "org.typelevel"     %% "cats-core"                      % "2.3.0",
     "uk.gov.hmrc"       %% "domain"                         % "8.1.0-play-28"
   )
 
