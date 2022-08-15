5c1adc524a11643c59e1328dac31392fa0944266
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-08-15 09:59:43 +0100

    Add Check Property page

---
+++ app/controllers/CheckPropertyController.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import com.google.inject.Inject
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import models.{Index, NormalMode}
+import navigation.Navigator
+import pages.CheckPropertyPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewmodels.checkAnswers.{PropertyAddressSummary, ShareOfPropertySummary}
+import viewmodels.govuk.summarylist._
+import views.html.CheckPropertyView
+
+class CheckPropertyController @Inject()(
+                                            override val messagesApi: MessagesApi,
+                                            identify: IdentifierAction,
+                                            getData: DataRetrievalAction,
+                                            requireData: DataRequiredAction,
+                                            navigator: Navigator,
+                                            val controllerComponents: MessagesControllerComponents,
+                                            view: CheckPropertyView
+                                          ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad(index: Index): Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+
+      val list = SummaryListViewModel(
+        rows = Seq(
+          PropertyAddressSummary.row(request.userAnswers, index),
+          ShareOfPropertySummary.row(request.userAnswers, index)
+        ).flatten
+      )
+
+      Ok(view(list, index))
+  }
+
+  def onSubmit(index: Index): Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      Redirect(navigator.nextPage(CheckPropertyPage(index), NormalMode, request.userAnswers))
+  }
+}
---
+++ test/controllers/CheckPropertyControllerSpec.scala
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import models.{Address, Index}
+import pages.{PropertyAddressPage, ShareOfPropertyPage}
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import viewmodels.checkAnswers.{PropertyAddressSummary, ShareOfPropertySummary}
+import viewmodels.govuk.SummaryListFluency
+import views.html.CheckPropertyView
+
+class CheckPropertyControllerSpec extends SpecBase with SummaryListFluency {
+
+  private val index = Index(0)
+
+  "Check Property Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val address = Address("line 1", None, "town", None, "postcode")
+      val share = 1
+
+      val answers =
+        emptyUserAnswers
+          .set(PropertyAddressPage(index), address).success.value
+          .set(ShareOfPropertyPage(index), share).success.value
+
+      val application = applicationBuilder(userAnswers = Some(answers)).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.CheckPropertyController.onPageLoad(index).url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[CheckPropertyView]
+
+        val list =
+          SummaryListViewModel(Seq(
+            PropertyAddressSummary.row(answers, index)(messages(application)),
+            ShareOfPropertySummary.row(answers, index)(messages(application))
+          ).flatten)
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(list, index)(request, messages(application)).toString
+      }
+    }
+
+    "must redirect to Journey Recovery for a GET if no existing data is found" in {
+
+      val application = applicationBuilder(userAnswers = None).build()
+
+      running(application) {
+        val request = FakeRequest(GET, routes.CheckYourAnswersController.onPageLoad.url)
+
+        val result = route(application, request).value
+
+        status(result) mustEqual SEE_OTHER
+        redirectLocation(result).value mustEqual routes.JourneyRecoveryController.onPageLoad().url
+      }
+    }
+  }
+}
